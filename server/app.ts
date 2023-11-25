6e8bdc21ab108d40640690e89fbefb8fad56223f
AliBile <ali.bile98@gmail.com>
2023-11-26 00:33:28 +0300

    db connections added

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express,{ NextFunction, Request, Response } from 'express';
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
